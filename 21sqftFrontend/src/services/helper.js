51f0a90a954252a56bcc0fd21f2c1694f17875a0
keshav189 <kes99101@gmail.com>
2024-05-28 23:24:47 +0530

    change backend url

--- 21sqftFrontend/src/services/helper.js
+++ 21sqftFrontend/src/services/helper.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://two1sqftbackend.onrender.com/api/v1';
+export const BASE_URL = 'https://sqftbackend-i52w4.ondigitalocean.app/api/v1';
 // export const BASE_URL = 'http://localhost:8080/api/v1';
